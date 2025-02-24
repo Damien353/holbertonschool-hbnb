925fedffe886441dbcab78aed7ee19113d4ce157
Coco29dev <10059@holbertonstudents.com>
2025-02-24 15:27:45 +0100

    modification user.py

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+import re
 from models.BaseModel import BaseModel
 
 
@@ -8,3 +10,15 @@ class User(BaseModel):
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
+        self.validate_email(email)
+        self.validate_name(first_name, last_name)
+
+        def validate_email(self, email):
+            email_regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+            if not re.match(email_regex, email):
+                raise ValueError("Email invalide")
+
+        def validate_name(self, first_name, last_name):
+            if len(first_name) > 50 or len(last_name) > 50:
+                raise ValueError(
+                    "Le nom ou prénom ne doit pas dépasser 50 caractères.")
