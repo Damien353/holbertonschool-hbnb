d4f310cb895fc8bf2aa0114f35aa0a7369640729
Coco29dev <10059@holbertonstudents.com>
2025-02-24 15:29:36 +0100

    indentation

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -13,12 +13,12 @@ class User(BaseModel):
         self.validate_email(email)
         self.validate_name(first_name, last_name)
 
-        def validate_email(self, email):
-            email_regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-            if not re.match(email_regex, email):
-                raise ValueError("Email invalide")
+    def validate_email(self, email):
+        email_regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        if not re.match(email_regex, email):
+            raise ValueError("Email invalide")
 
-        def validate_name(self, first_name, last_name):
-            if len(first_name) > 50 or len(last_name) > 50:
-                raise ValueError(
-                    "Le nom ou prénom ne doit pas dépasser 50 caractères.")
+    def validate_name(self, first_name, last_name):
+        if len(first_name) > 50 or len(last_name) > 50:
+            raise ValueError(
+                "Le nom ou prénom ne doit pas dépasser 50 caractères.")
