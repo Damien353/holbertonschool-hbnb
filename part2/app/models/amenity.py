6babb69ca913a03f195cc3363855612af78d0bdf
Coco29dev <10059@holbertonstudents.com>
2025-02-24 18:23:31 +0100

    creation amenity

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
 from models.BaseModel import BaseModel
+
+
+class Amenity(BaseModel):
+    def __init__(self, name: str):
+        super().__init__()
+        if not name or len(name) > 50:
+            raise ValueError(
+                "Le nom de l'amenity ne doit pas être vide et doit comporter au maximum 50 caractères.")
+        self.name = name
