5b78404391085107c6edc33d6e1ddac8537cd817
Coco29dev <10059@holbertonstudents.com>
2025-02-25 09:57:29 +0100

    rajout description amenity

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -3,9 +3,10 @@ from models.BaseModel import BaseModel
 
 
 class Amenity(BaseModel):
-    def __init__(self, name: str):
+    def __init__(self, name: str, description: str = ""):
         super().__init__()
         if not name or len(name) > 50:
             raise ValueError(
                 "Le nom de l'amenity ne doit pas être vide et doit comporter au maximum 50 caractères.")
         self.name = name
+        self.description = description
