d2c2e2ab6dd664b2efb95c58a88ea8e039acd2b3
Coco29dev <10059@holbertonstudents.com>
2025-02-26 09:20:32 +0100

    suppression repository objet

--- part2/app/persistence/amenity_repository.py
+++
@@ -1,9 +0,0 @@
-from app.persistence.repository import InMemoryRepository
-
-
-class AmenityRepository(InMemoryRepository):
-    def __init__(self):
-        super().__init__()
-
-    def find_by_name(self, name):
-        return self.get_by_attribute("name", name)
--- part2/app/persistence/place_repository.py
+++
@@ -1,9 +0,0 @@
-from app.persistence.repository import InMemoryRepository
-
-
-class PlaceRepository(InMemoryRepository):
-    def __init__(self):
-        super().__init__()
-
-    def find_by_location(self, location):
-        return self.get_by_attribute("location", location)
